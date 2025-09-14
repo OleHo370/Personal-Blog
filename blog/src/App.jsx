ba56a83520650c346a8c6ceba108500c05582676
Ole Ho <oleho370@gmail.com>
2025-09-14 02:39:51 -0400

    Converted website into React and used Vite, updated code to be in JSX

---
+++ blog/src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from './Nav';
+import './styles.css';
+
+
+export default function App() {
+  return (
+    <>
+      <Nav />
+      <div className="page">
+      </div>
+    </>
+  );
+}
---
+++ blog/src/Nav.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export default function Nav() {
+  const stateArr = useState(false);
+  const active = stateArr[0];
+  const setActive = stateArr[1];
+
+  function toggleMenu() {
+    setActive(!active);
+  };
+  
+
+  return (
+    <nav className="nav">
+      <div className="topmenu">
+        <a href="/" className="homelogo">
+          <i className="bi bi-house-fill"></i> Olé Ho
+        </a>
+
+        <ul className={"tablist" + (active ? "active" : "")}>
+          <li className="tab">
+            <a href="/" className="tablink">My Projects</a>
+          </li>
+          <li className="tab">
+            <a href="/aboutme.html" className="tablink">About Me</a>
+          </li>
+          <li className="contactbutton">
+            <a href="/" className="button">Contact Me</a>
+          </li>
+        </ul>
+
+        <i className={`bi ${active ? 'bi-x-square' : 'bi-list'} hamburgerlist`} onClick={toggleMenu}></i>
+      </div>
+    </nav>
+  );
+}
