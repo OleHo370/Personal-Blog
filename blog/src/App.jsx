5bf3f19273b6a6aecc5ed52c05c89c16ae86d41f
Ole Ho <oleho370@gmail.com>
2025-09-25 03:00:49 -0400

    Added images to each project, made the UI when the screen is shrunk more consistent

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,5 +1,8 @@
 import Nav from './Nav';
 import './styles.css';
+import prescriptionSampleImg from './assets/images/prescriptionsample.png';
+import drivingSampleImg from './assets/images/drivingsample.png';
+import drumSampleImg from './assets/images/drumsample.png';
 
 export default function App() {
 
@@ -21,47 +24,67 @@ export default function App() {
             <div className="projectlist">
 
               <div className="projectbox">
-                <h3>Prescription Manager App</h3>
-                <p>test description ijodsfijbfoapkjnfds</p>
-                <div className="skillslist">
-                  <span className="skill">JavaScript</span>
-                  <span className="skill">CSS</span>
-                  <span className="skill">HTML</span>
-                  <span className="skill">Swift</span>
-                  <span className="skill">AWS</span>
-                  <span className="skill">Figma</span>
+                <div className="projecttext">
+                  <h3>Prescription Manager App</h3>
+                  <p>test description ijodsfijbfoapkjnfds</p>
+                  <div className="skillslist">
+                    <span className="skill">JavaScript</span>
+                    <span className="skill">CSS</span>
+                    <span className="skill">HTML</span>
+                    <span className="skill">Swift</span>
+                    <span className="skill">AWS</span>
+                    <span className="skill">Figma</span>
+                  </div>
+                </div>
+                <div className="projectimage">
+                  <img src={prescriptionSampleImg}/>
                 </div>
               </div>
 
               <div className="projectbox">
-                <h3>Ole's Blog</h3>
-                <p>test description ijodsfijbfoapkjnfds</p>
-                <div className="skillslist">
-                  <span className="skill">React</span>
-                  <span className="skill">JavaScript</span>
-                  <span className="skill">CSS</span>
-                  <span className="skill">HTML</span>
+                <div className="projecttext">
+                  <h3>Olé's Blog</h3>
+                  <p>test description ijodsfijbfoapkjnfds</p>
+                  <div className="skillslist">
+                    <span className="skill">React</span>
+                    <span className="skill">JavaScript</span>
+                    <span className="skill">CSS</span>
+                    <span className="skill">HTML</span>
+                  </div>
+                </div>
+                
+              </div>
+
+              <div className="projectbox">
+                <div className="projecttext">
+                  <h3>Maze-Navigating Robot Car</h3>
+                  <p>test description ijodsfijbfoapkjnfds</p>
+                  <div className="skillslist">
+                    <span className="skill">C++</span>
+                    <span className="skill">Arduino</span>
+                    <span className="skill">CAD</span>
+                  </div>
+                </div>
+                <div className="projectimage">
+                  <img src={drivingSampleImg}/>
                 </div>
               </div>
 
               <div className="projectbox">
-                <h3>Maze-Navigating Arduino Robot</h3>
-                <p>test description ijodsfijbfoapkjnfds</p>
-                <div className="skillslist">
-                  <span className="skill">C++</span>
-                  <span className="skill">Arduino</span>
-                  <span className="skill">CAD</span>
+                <div className="projecttext">
+                  <h3>Drum-Playing Robot</h3>
+                  <p>test description ijodsfijbfoapkjnfds</p>
+                  <div className="skillslist">
+                    <span className="skill">C++</span>
+                    <span className="skill">Arduino</span>
+                    <span className="skill">CAD</span>
+                  </div>
                 </div>
-              </div>
-
-              <div className="projectbox">
-                <h3>Drum-Playing Robot</h3>
-                <p>test description ijodsfijbfoapkjnfds</p>
-                <div className="skillslist">
-                  <span className="skill">C++</span>
-                  <span className="skill">Arduino</span>
-                  <span className="skill">CAD</span>
+                <div className="projectimage">
+                  <img src={drumSampleImg}/>
                 </div>
+                
+                
               </div>
 
 
